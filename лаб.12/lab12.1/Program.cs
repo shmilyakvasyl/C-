754396bad4500bac093e05f48b82d1e97198bd78
shmilyakvasyl <74256836+shmilyakvasyl@users.noreply.github.com>
2021-05-23 22:58:24 +0300

    Add files via upload

---
+++ лаб.12/lab12.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class TMoney
+    {
+        public double Value { get { return value; } }
+
+        public const double value = 28.01;
+
+        public string Add()
+        {
+            Console.Write("UAH1 add:");
+            double UAH1 = double.Parse(Console.ReadLine());
+            Console.Write("to UAH2:");
+            double UAH2 = double.Parse(Console.ReadLine());
+            double USD1 = UAH1 / value;
+            double USD2 = UAH2 / value;
+            double sum = USD1 + USD2;
+            if (UAH1 > 0 & UAH2 > 0)
+            {
+                return $"{sum}$";
+            }
+            else
+            {
+                return "Should be positive!";
+            }
+        }
+
+        public string Sub()
+        {
+            Console.Write("UAH1 sub:");
+            double UAH1 = double.Parse(Console.ReadLine());
+            Console.Write("from UAH2:");
+            double UAH2 = double.Parse(Console.ReadLine());
+            double USD1 = UAH1 / value;
+            double USD2 = UAH2 / value;
+            double subtraction = USD2 - USD1;
+            if (UAH1 > 0 & UAH2 > 0)
+            {
+                return $"{subtraction}$";
+            }
+            else
+            {
+                return "Should be positive!";
+            }
+        }
+
+        public double CourseGain()
+        {
+            return value + 100;
+        }
+    }
+
+}
---
+++ лаб.12/lab12.2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+
+{
+    class Array
+    {
+        private double[] array;
+
+        private double[] ArraY
+        {
+            get
+            {
+                return array;
+            }
+            set
+            {
+                array = value;
+            }
+        }
+
+        public void IntroductionOfElements()
+        {
+            Console.WriteLine("How many elements u want?");
+            int n = int.Parse(Console.ReadLine());
+            array = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter a {i + 1} element: ");
+                double el = Convert.ToDouble(Console.ReadLine());
+                array[i] = el;
+            }
+
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        public double MaxOfElements()
+        {
+            double max = int.MinValue;
+            for (int i = 1; i < array.GetLength(0); i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        public double MinOfElements()
+        {
+            double min = int.MaxValue;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+    }
+
+}
