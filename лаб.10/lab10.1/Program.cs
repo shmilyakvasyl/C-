8eb1feefc50f89174cda85ef75fce3b1229cc6b7
shmilyakvasyl <74256836+shmilyakvasyl@users.noreply.github.com>
2021-05-23 22:56:50 +0300

    Add files via upload

---
+++ лаб.10/lab10.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task1
+{
+    public class Text
+    {
+        public string str = "Здоровенькi були! Цей текст був написаний 25.04.2021";
+        public string subject = "Розваги/Юмор";
+
+        public uint Count()
+        {
+            uint count = 0;
+            foreach (var el in str) if (char.IsLetter(el))
+                    count++;
+            return count;
+        }
+
+        public void Space()
+        {
+            int f = str.Split(' ').Length - 1;
+            Console.WriteLine(f);
+        }
+
+        public void Change()
+        {
+            Console.WriteLine(str);
+            Console.WriteLine(str.Replace("а", "у"));
+        }
+
+        public void Remove()
+        {
+            string[] text;
+            text = str.Split(' ');
+            //Console.WriteLine(text.Length);
+            Console.Write("Index:");
+            int index = int.Parse(Console.ReadLine());
+            string word = text[index];
+            string s = str.Replace(word, "");
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ лаб.10/lab10.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Task2
+{
+    class MobileTariff
+    {
+        public const string name = "Для всiєї сiм'ї";
+        public DateTime date_start = new DateTime(2021, 3, 20);
+        public DateTime date_end = new DateTime(2022, 3, 20);
+        public int subscription_fee = 150;
+        public double cost_per_minute = 1.5;
+
+        public TimeSpan DataFind()
+        {
+            TimeSpan date_sub = date_end.Subtract(date_start);
+            return date_sub;
+        }
+
+        public double PricingForACall()
+        {
+            Console.Write("Скiльки Вам потрiбно хвилин для дзвiнка?:");
+            uint call = uint.Parse(Console.ReadLine());
+            return call * cost_per_minute;
+        }
+
+        public long SubscriptionFeeInAdvance()
+        {
+            Console.Write("На скiльки мiсяцiв Ви хочете взяти абонплату?:");
+            uint subscription = uint.Parse(Console.ReadLine());
+            int action = Convert.ToInt32(date_end.Subtract(date_start));
+            while (subscription > action)
+            {
+                Console.Write($"Лихо! Тариф дiє тiльки {date_end.Subtract(date_start)}");
+                Console.Write("На скiльки мiсяцiв Ви хочете взяти абонплату?:");
+                subscription = uint.Parse(Console.ReadLine());
+            }
+            return subscription_fee * subscription;
+        }
+    }
+}
